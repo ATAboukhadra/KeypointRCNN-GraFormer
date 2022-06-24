5bb71531d0fa8b0b92801adbd7c2eca3bcbb214b
Ahmed Aboukhadra <ahmed.tawfik96@gmail.com>
2022-06-24 12:05:24 +0200

    install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-conda create -n pytorch3d python=3.8
-conda activate pytorch3d
-conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
-conda install -c fvcore -c iopath -c conda-forge fvcore iopath
-conda install -c bottler nvidiacub
-conda install pytorch3d -c pytorch3d
 pip install -r requirements.txt
 
 
