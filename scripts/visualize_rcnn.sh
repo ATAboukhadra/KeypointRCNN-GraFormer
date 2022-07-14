9b500e853cfc25ae5f6ef0c257cf22f26ec66528
Ahmed Aboukhadra <ahmed.tawfik96@gmail.com>
2022-07-14 16:55:57 +0200

    local

--- scripts/visualize_rcnn.sh
+++ scripts/visualize_rcnn.sh
@@ -3,11 +3,13 @@ export CUDA_DEVICE_ORDER=PCI_BUS_ID
 python3 vis_rcnn.py \
  --root ../HOPE/datasets/ho-v3-mesh/ \
  --ycb_path ./datasets/ycb_models/ \
- --checkpoint_folder ho-rcnn-hand \
- --checkpoint_id 3 \
+ --checkpoint_folder photometric-2048 \
+ --checkpoint_id 2 \
  --gpu \
  --gpu_number 1 \
- --split val \
+ --split train \
  --seq rgb \
  --graformer \
-#  --visualize
+ --object \
+ --generate_mesh \
+ --visualize
