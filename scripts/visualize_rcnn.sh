e69a6d1bbfd25a93b8ba621cec6b62556a91d6f3
Ahmed Aboukhadra <ahmed.tawfik96@gmail.com>
2022-07-15 11:42:19 +0200

    vis

--- scripts/visualize_rcnn.sh
+++ scripts/visualize_rcnn.sh
@@ -4,12 +4,12 @@ python3 vis_rcnn.py \
  --root ../HOPE/datasets/ho-v3-mesh/ \
  --ycb_path ./datasets/ycb_models/ \
  --checkpoint_folder photometric-2048 \
- --checkpoint_id 2 \
+ --checkpoint_id 10 \
  --gpu \
  --gpu_number 1 \
- --split train \
+ --split val \
  --seq rgb \
  --graformer \
  --object \
  --generate_mesh \
- --visualize
+#  --visualize
